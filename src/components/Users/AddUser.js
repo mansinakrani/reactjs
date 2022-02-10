c9c9bfef268c0ac0caee558bf925d8f112e24f89
mansinakrani <mansinakrani006@gmail.com>
2022-02-10 16:48:21 +0530

    Modified Adduser.js for managing user input state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+          //[hold the latest state snapshot ,holds afunction which we can call]
+    const [enteredUsername, setEnteredUsername] = useState(''); // array de-structuring (useState always returns an array with exactly two elements)
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
-        event.preventDefault();
+        event.preventDefault(); // entered value utilize here
+        console.log(enteredUsername, enteredAge);
     };
-  return (
-    <Card className={classes.input}>
-        <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <Button type="submit">Add User</Button>
-        </form>
-    </Card>
-  );
+
+    //function which is triggered on every keystroke of that input here.
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    };
+
+    return (
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" onChange={usernameChangeHandler}/>
+            <label htmlFor="age">Age (Years)</label>
+            <input id="age" type="number" onChange={ageChangeHandler} />
+            <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    );
 };
 
 export default AddUser;
