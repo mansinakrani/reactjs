1f78898375ecdb045bdc926b87f2ed27daca7b73
mansinakrani <mansinakrani006@gmail.com>
2022-02-12 10:35:58 +0530

    edited Wrapper.js file

---
+++ src/components/Helpers/Wrapper.js
@@ -0,0 +1,7 @@
+//Did't import React - not gone write ant JSX code.
+
+const Wrapper = props => {
+    return props.children; // holds all content passed between opening & closing tag
+};
+
+export default Wrapper;
