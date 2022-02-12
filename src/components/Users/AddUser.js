fd401b3598cefc2d5141b87a81bca4e39426e986
mansinakrani <mansinakrani006@gmail.com>
2022-02-12 15:05:33 +0530

    Used ref's

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -7,43 +7,37 @@ import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {  
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   //[hold the latest state snapshot ,holds afunction which we can call]
-  const [enteredUsername, setEnteredUsername] = useState(""); // array de-structuring (useState always returns an array with exactly two elements)
-  const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault(); // entered value utilize here
-
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
     //only executes if we have valid inputs
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid Input",
         message: "please enter a valid nam e and age (non-empty values).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
         message: "please enter a valid age (> 0).",
       });
       return;
     } // force a conversion of entered age to a number by adding a plus
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername(""); //resetting
-    setEnteredAge("");
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = ''; // Rarely use refs too manipulate the DOM.
+    ageInputRef.current.value = '';
   };
 
   //function which is triggered on every keystroke of that input here.
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
-
   const errorHandler = () => {
     setError(null);
   };
@@ -63,15 +57,13 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
